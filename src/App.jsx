113aa18cf1b4e623329135567aba4a78e36f5232
Temism <marcopalmasoto@gmail.com>
2024-07-08 10:52:17 -0400

    footer

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,5 @@
 import Header from "./components/Header/Header";
+import Footer from "./components/Footer/Footer";
 
 function App() {
   return (
@@ -10,6 +11,7 @@ function App() {
         contacto="Contacto"
       />
       <main></main>
+      <Footer />
     </>
   );
 }
