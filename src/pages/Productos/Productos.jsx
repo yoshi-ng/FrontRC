b31f845f6a65e2c05a9de41ab8541cca71736678
Valita <valentinamardonesaravena@gmail.com>
2024-07-10 15:42:46 -0400

    busqueda elementos OK  | Banner emprendedor avance

--- src/pages/Productos/Productos.jsx
+++ src/pages/Productos/Productos.jsx
@@ -1,35 +1,12 @@
-import { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
-import "./Productos.css";
-import data from "../../components/assets/all_product";
-import clp from "../../components/Clp";
+import Producto from "../../components/Producto/Producto";
 
 function Productos() {
   const { id } = useParams();
-  const [itemProducto, setItemProducto] = useState({ id });
-  useEffect(() => {
-    const items = data.find((prod) => prod.id == id);
-    setItemProducto(items);
-  }, []);
-
   return (
-    <>
-      <div className="contenedor_item">
-        <div className="contenedor_imagen">
-          <img src={itemProducto.imagen} alt={itemProducto.descripcion} />
-        </div>
-
-        <div className="contenedor_texto">
-          <h2 className="nombre">{itemProducto.nombre}</h2>
-          <div className="detalle">
-            <p className="titulo">Descripcion del producto:</p>
-            <p className="descripcion">{itemProducto.descripcion}</p>
-          </div>
-          <div className="stock">{itemProducto.stock} unidades disponibles</div>
-          <div className="precio">{clp(itemProducto.precio)}</div>
-        </div>
-      </div>
-    </>
+    <div key={id}>
+      <Producto id={id} />
+    </div>
   );
 }
 
