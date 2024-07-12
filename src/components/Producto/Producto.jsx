fb403c8c89e6c80397bd6c1159a681377a966e6f
Valita <valentinamardonesaravena@gmail.com>
2024-07-12 05:27:30 -0400

    tarjetas productos item OK

--- src/components/Producto/Producto.jsx
+++ src/components/Producto/Producto.jsx
@@ -13,18 +13,22 @@ function Productos({ id }) {
   return (
     <>
       <div className="contenedor_item">
-        <div className="contenedor_imagen">
-          <img src={itemProducto.imagen} alt={itemProducto.descripcion} />
-        </div>
+        <img
+          className="contenedor_item_imagen"
+          src={itemProducto.imagen}
+          alt={itemProducto.descripcion}
+        />
 
-        <div className="contenedor_texto">
-          <h2 className="nombre">{itemProducto.nombre}</h2>
-          <div className="detalle">
-            <p className="titulo">Descripcion del producto:</p>
-            <p className="descripcion">{itemProducto.descripcion}</p>
+        <div className="contenedor_item_texto">
+          <h2 className="item_nombre">{itemProducto.nombre}</h2>
+          <div className="item_detalle">
+            <p className="item_titulo">Descripcion del producto:</p>
+            <p className="item_descripcion">{itemProducto.descripcion}</p>
           </div>
-          <div className="stock">{itemProducto.stock} unidades disponibles</div>
-          <div className="precio">{clp(itemProducto.precio)}</div>
+          <div className="item_precio">{clp(itemProducto.precio)}</div>
+          <div className="item_stock">
+            {itemProducto.stock} unidades disponibles
+          </div>
         </div>
       </div>
     </>
